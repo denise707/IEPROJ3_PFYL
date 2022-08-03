57de3d69d17b3c1df77b84e667d61d88b8bf1226
CesarManalac <cesar_manalac@dlsu.edu.ph>
2022-08-03 15:42:51 +0800

    updated ground items to reflect the scriptable obj assigned

--- Assets/Scripts/Inventory/GroundItem.cs
+++ Assets/Scripts/Inventory/GroundItem.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GroundItem : MonoBehaviour
+using UnityEditor;
+public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
 {
     public ItemObject item;
+
+    public void OnAfterDeserialize()
+    {
+        
+    }
+
+    public void OnBeforeSerialize()
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
+        EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
+    }
 }
