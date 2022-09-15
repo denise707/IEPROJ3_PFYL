8186549761a2f857797741d8241e530387721353
denise_taiza <maria_alyssa_taiza@dlsu.edu.ph>
2022-09-16 00:11:44 +0800

    Updated UI (WIP)

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -11,8 +11,18 @@ public class InGameUIManager : MonoBehaviour
     private float accumMins = 0.0f;
     private float TIME_MULTIPLIER = 60.0f; // 3f for debugging // 2.0f normal
 
+    [Header("HUD Objects")]
     [SerializeField] Transform clock;
     [SerializeField] Text dayLabel;
+
+    [Header("Blocker Panels")]
+    [SerializeField] private GameObject blocker1;
+    [SerializeField] private GameObject blocker2;
+
+    [Header("Pop Up Panels")]
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject mainMenuConfirmation;
 
     private void Awake()
     {
@@ -69,5 +79,56 @@ public class InGameUIManager : MonoBehaviour
     public void UpdateDayLabel(String day)
     {
         this.dayLabel.text = day;
+    }
+
+    public void PauseMenu()
+    {
+        HandlePopUp(pauseMenu, blocker1);
+    }
+
+    public void OptionsMenu()
+    {
+        HandlePopUp(optionsMenu, blocker2);
+    }
+
+    public void MainMenuConfirmation()
+    {
+        HandlePopUp(mainMenuConfirmation, blocker2);
+    }
+
+    public void HandlePopUp(GameObject popUp, GameObject blocker)
+    {
+        if (popUp.activeInHierarchy == false)
+        {
+            popUp.SetActive(true);
+            popUp.GetComponent<PanelOpener>().OpenPanel();
+        }
+
+        else
+        {
+            popUp.GetComponent<PanelOpener>().OpenPanel();
+        }
+
+        if (blocker.activeInHierarchy == false)
+        {
+            BlockerfadeIn(blocker);
+        }
+
+        else
+        {
+            BlockerfadeOut(blocker);
+        }
+    }
+
+
+    public void BlockerfadeIn(GameObject obj)
+    {
+        obj.SetActive(true);
+        obj.GetComponent<FadeVFX>().panelState = FadeVFX.PanelState.FadeIn;
+    }
+
+    public void BlockerfadeOut(GameObject obj)
+    {
+        obj.GetComponent<FadeVFX>().panelState = FadeVFX.PanelState.FadeOut;
     }
 }
