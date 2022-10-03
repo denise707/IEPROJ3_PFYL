c341b9cab53058c60fd3d5e140b6cca2c167a96e
denise_taiza <maria_alyssa_taiza@dlsu.edu.ph>
2022-10-03 22:40:18 +0800

    Update: currency mechanic + win/lose condition

--- Assets/Scripts/Enemy/TimeManager.cs
+++ Assets/Scripts/Enemy/TimeManager.cs
@@ -9,6 +9,7 @@ public class TimeManager : MonoBehaviour
     private TimeState currTime = TimeState.DayTime;
 
     public int day = 1;
+    private bool isLastDay = false;
 
     public float dayHour = 0; // set to 5 for debugging
     private float dayMinute = 0.0f;
@@ -16,11 +17,11 @@ public class TimeManager : MonoBehaviour
     public float nightHour = 0; // set to 5 for debugging
     private float nightMinute = 0.0f;
 
-    private const int maxDay = 5; // one full week cycle
+    private const int maxDay = 6; // one full week cycle
     public float maxHours = 2.0f; // hours it takes to be considered as a Day (2 hours = 1 minute irl)
     public float maxMins = 60.0f; // mins it takes to be considered as an hour
 
-    private float TIME_MULTIPLIER = 2.0f; // --- 60.0f for debugging --- 2.0f normal ---
+    private float TIME_MULTIPLIER = 60.0f; // --- 60.0f for debugging --- 2.0f normal ---
 
     private void Awake()
     {
@@ -54,11 +55,18 @@ public class TimeManager : MonoBehaviour
             PlayNightTicks();
             UpdateNightTime();
         }
+
+        if (day >= 6 && !isLastDay)
+        {
+            GameManager.instance.gameState = GameManager.GameState.Win;
+            isLastDay = true;
+        }
     }
 
     private void UpdateTimeState()
     {
         InGameUIManager.instance.UpdateDayLabel("Day " + day);
+        if (day >= 5) { InGameUIManager.instance.UpdateDayLabel("Day " + 5); }
 
         if (dayHour == maxHours && currTime == TimeState.DayTime) // set to night when the hours needed is met
         {
