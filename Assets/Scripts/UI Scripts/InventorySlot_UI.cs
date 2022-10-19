6f51ccdd78de115bf79ad86493e832f20fbb64fc
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2022-10-19 17:05:51 +0800

    updated game manager and fixed infinite see bug

--- Assets/Scripts/UI Scripts/InventorySlot_UI.cs
+++ Assets/Scripts/UI Scripts/InventorySlot_UI.cs
@@ -68,6 +68,8 @@ public class InventorySlot_UI : MonoBehaviour
         if(assignedInventorySlot.StackSize <= 0)
         {
             ClearSlot();
+            HotbarSelectorManager.instance.UpdatePlayerEquip();
+
         }
         else
         {
