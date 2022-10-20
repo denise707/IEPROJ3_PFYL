026cfd29fe6c6fe65bf6e148d52018c0254404d1
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2022-10-20 22:00:43 +0800

    created buke plant prefab

---
+++ Assets/Scripts/Planting/NukePlantBehavior.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukePlantBehavior : MonoBehaviour
+{
+    public enum PlantPhase
+    {
+        Phase0_Default,
+        Phase1_Seedling,
+        Phase2_MidGrown,
+        Phase3_FullyGrown
+    }
+
+    [Header("Plant Properties")]
+    [SerializeField] InventoryItemData plantToGrow;
+
+    public PlantPhase plantStatus = PlantPhase.Phase0_Default;
+    [SerializeField] int phase = 0;
+    public bool simulateGrowth = false;
+    [SerializeField] bool isGrowing = false;
+
+
+    public float ticks = 0f;
+
+    [Header("Plant Growth Sprite")]
+    [SerializeField] List<Sprite> GrowthSpriteList;
+
+    [Header("Particle Sprite")]
+    [SerializeField]ParticleSystem particle;
+
+    Animator animator;
+    Soil soil;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (plantToGrow)
+        {
+            PlantSeed(plantToGrow);
+        }
+
+        soil = gameObject.GetComponentInParent<Soil>();
+        animator = gameObject.GetComponent<Animator>();
+
+        if (!animator)
+        {
+            Debug.Log("Cannot Find animator");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (simulateGrowth && !isGrowing)
+        {
+            EnablePlantGrowth();
+            isGrowing = true;
+        }
+    }
+
+    public void PlantSeed(InventoryItemData plantObj)
+    {
+        this.gameObject.transform.localPosition = gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, 0.5f, gameObject.transform.localPosition.z);
+        Debug.Log("Updating Plant Stats");
+        this.GrowthSpriteList = plantObj.PlantGrowthSpriteList;
+        this.phase = 0;
+
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = GrowthSpriteList[phase]; // seed
+        plantStatus = PlantPhase.Phase1_Seedling;
+    }
+
+    public void EnablePlantGrowth()
+        {
+            Debug.Log("Start Growing");
+            //ticks = 0;
+            isGrowing = true;
+
+
+        }
+
+    public void NextPlantPhase()
+    {
+        if (phase >= GrowthSpriteList.Count)
+        {
+            return;
+            Debug.Log("Fully Grown");
+        }
+        phase++;
+        Debug.Log(phase);
+
+
+        if (phase == 1)
+        {
+            this.gameObject.transform.localPosition = gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, 0.80f, gameObject.transform.localPosition.z);
+            plantStatus = PlantPhase.Phase2_MidGrown;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = GrowthSpriteList[phase];
+
+        }
+        else if (phase == 2)
+        {
+            plantStatus = PlantPhase.Phase3_FullyGrown;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = GrowthSpriteList[phase];
+
+            PrepareForHarvest();
+
+            isGrowing = false;
+            simulateGrowth = false;
+        }
+        
+    }
+
+    void PrepareForHarvest()
+    {
+        if (soil)
+        {
+
+            soil.soilStatus = Soil.SoilStatus.ForHarvesting;
+            animator.SetBool("isFullyGrown", true);
+
+            // show and play particle
+            particle.gameObject.SetActive(true);
+            particle.Play();
+        }
+        else
+        {
+            Debug.Log("not found");
+
+        }
+        // enable particle
+
+    }
+
+    public void HarvestPlant()
+    {
+        this.gameObject.transform.localPosition = gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, 0.5f, gameObject.transform.localPosition.z);
+        Debug.Log("Reset Plant Stats");
+
+        this.phase = 0;
+        plantStatus = PlantPhase.Phase0_Default;
+        animator.SetBool("isFullyGrown", false);
+
+        // stop and hide particle
+        particle.Stop();
+        particle.gameObject.SetActive(true);
+
+    }
+
+    public void InitizalizeDrops()
+    {
+       
+    }
+}
