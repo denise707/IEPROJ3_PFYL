e474d9944a5286c164869e6e68bece9f8a510579
denise_taiza <maria_alyssa_taiza@dlsu.edu.ph>
2022-12-11 00:50:25 +0800

    Added dagger and bomb mechanic

---
+++ Assets/Scripts/Player/CabbageBomb.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabbageBomb : MonoBehaviour
+{
+    [SerializeField] GameObject parent;
+    [SerializeField] GameObject mark;
+    List<EnemyBehaviour> trackedEnemies = new List<EnemyBehaviour>();
+    [SerializeField] GameObject vfx;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Enemy")
+        {
+            trackedEnemies.Add(collider.gameObject.GetComponent<EnemyBehaviour>());
+            mark.SetActive(true);
+            StartCoroutine(Explode());
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Enemy")
+        {
+            trackedEnemies.Remove(collider.gameObject.GetComponent<EnemyBehaviour>());
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(2f);
+        vfx.GetComponent<ParticleSystem>().Play();
+        for (int i = 0; i < trackedEnemies.Count; i++)
+        {
+            trackedEnemies[i].ReceiveDamage(CombatManager.instance.damage);
+        }
+        Destroy(parent, 0.2f);
+    }
+}
