c4a3a639022b54ffd4058bcbf3c8d7f68c3fe33d
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2022-12-11 13:29:20 +0800

    debugged camera error and adjusted plant sprite size

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -10,7 +10,10 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
     }
 
     // Update is called once per frame
